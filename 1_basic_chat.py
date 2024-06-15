ee0f41507b40d64ddba377a93eedc5ca492bd002
M.J. Hill <mjh2225@gmail.com>
2024-06-15 12:06:11 -0400

    completed demo 1

--- 1_basic_chat.py
+++ 1_basic_chat.py
@@ -1,1 +1,14 @@
 # Query the llama 3 model.
+import ollama
+
+user_message = input("Enter a prompt:")
+
+stream = ollama.chat(model='llama3', messages=[{
+    'role': 'user',
+    'content': user_message,
+}], 
+stream=True
+)
+
+for chunk in stream:
+    print(chunk['message']['content'], end='', flush=True)
