0f28ac2167d1bd766b2d320c50ae95dce46aa2c3
M.J. Hill <mjh2225@gmail.com>
2024-06-15 12:21:08 -0400

    added demo 2

--- 3_langchain_chat.py
+++ 3_langchain_chat.py
@@ -4,3 +4,11 @@ from langchain_core.prompts import ChatPromptTemplate
 
 #Use Langchain to create Prompt Templates.  This can help with structuring prompts if you want your
 #LLM to only serve specific purposes such as telling jokes
+
+llm = ChatOllama(model='llama3')
+prompt = ChatPromptTemplate.from_template("Tell me a joke about {topic}")
+
+chain = prompt | llm | StrOutputParser()
+
+user_message = input("Enter a topic to make a joke about: ")
+print(chain.invoke({'topic': user_message}))
